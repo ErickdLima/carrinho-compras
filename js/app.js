1c5b5ed30c88ba959058690f048a2fb32408a0b0
ErickdLima <profissional.erick.lima@gmail.com>
2025-06-10 14:26:00 -0300

    mudanças no tratamento na verificação da quantidade

--- js/app.js
+++ js/app.js
@@ -8,37 +8,39 @@ function adicionar(){
 
     let produto = document.getElementById("produto").value;// variavel utilizada somente para declarar nomePrduto e valorProduto
 
-    let quantidade = document.getElementById("quantidade").value;   
+    let quantidade = document.getElementById("quantidade").value;
+    quantidade = Number(quantidade.trim());   
     let nomeProduto = produto.split('-')[0];
     let valorProduto = parseInt(produto.split('$')[1]);
     let valorTotal = 0;
     let objNomes = Object.keys(listaCarrinho);
     
-    if(quantidade == ''){
-        alert('Favor adicionar a quantidade');
+
+    
+    if(quantidade == ''|| !Number.isInteger(quantidade)||quantidade < 1 ){
+        alert('Favor adicionar a quantidade');       
+        return
     }
-    else{
-        //verifica se o produto ja esta no carrinho e se estiver aumenta somente o catador do produto
-        if(objNomes.includes(nomeProduto)){ 
 
-                nomeCarrinho.innerHTML = '';
-                for(let i = 0;i < objNomes.length;i++){
+    if(objNomes.includes(nomeProduto)){ 
+        
+        nomeCarrinho.innerHTML = '';
+        for(let i = 0;i < objNomes.length;i++){
 
-                    if(objNomes[i].includes(nomeProduto)){
-                        listaCarrinho[objNomes[i]].quantidade = parseInt(listaCarrinho[objNomes[i]].quantidade) + parseInt(quantidade);
-                    }
-                    nomeCarrinho.innerHTML += (`<section class="carrinho__produtos__produto"><span class="texto-azul">x${listaCarrinho[objNomes[i]].quantidade}</span> ${objNomes[i]}
-                    <span class="texto-azul">R$${listaCarrinho[objNomes[i]].valor}</span></section>`).trim();
-                        
+            if(objNomes[i].includes(nomeProduto)){
+                listaCarrinho[objNomes[i]].quantidade = parseInt(listaCarrinho[objNomes[i]].quantidade) + parseInt(quantidade);
             }
+            nomeCarrinho.innerHTML += (`<section class="carrinho__produtos__produto"><span class="texto-azul">x${listaCarrinho[objNomes[i]].quantidade}</span> ${objNomes[i]}
+            <span class="texto-azul">R$${listaCarrinho[objNomes[i]].valor}</span></section>`).trim();
+                        
         }
+    }
         //adiciona o produto ao carrinho e ao objeto(listaCarrinho)
-        else{
-            nomeCarrinho.innerHTML += (`<section class="carrinho__produtos__produto"><span class="texto-azul">x${quantidade}
-                </span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span></section>`).trim();
+    else{
+        nomeCarrinho.innerHTML += (`<section class="carrinho__produtos__produto"><span class="texto-azul">x${quantidade}
+            </span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span></section>`).trim();
 
-            listaCarrinho[nomeProduto] = {quantidade:quantidade, valor:valorProduto};
-        }
+        listaCarrinho[nomeProduto] = {quantidade:quantidade, valor:valorProduto};
     }
 
     // conta os itens e a quantidade no objeto(listaCarrinho) e define o valor 
